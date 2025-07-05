fda0cef2e5503a80b6fe7b7e650ae3d9ae2b3544
endgatesystemtechnologies <endgate.systems@protonmail.com>
2025-07-05 12:26:46 -0600

    Updatenew setup-postgres.sh

--- scripts/setup-postgres.sh
+++ scripts/setup-postgres.sh
@@ -8,9 +8,12 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y postgresql postgresql-contrib
 
 echo "📄 Configuring DB user and schema..."
 
+systemctl enable postgresql
+systemctl start postgresql
+
 sudo -u postgres psql <<EOF
-CREATE USER listmonk WITH PASSWORD 'listmonk';
-CREATE DATABASE listmonk OWNER listmonk;
+CREATE USER IF NOT EXISTS listmonk WITH PASSWORD 'listmonk';
+CREATE DATABASE IF NOT EXISTS listmonk OWNER listmonk;
 EOF
 
 echo "✅ PostgreSQL ready"
